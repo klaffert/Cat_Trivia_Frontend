c73bac713ab55f339dbb7a31f133c7ec362fec29
github username <github email address>
2019-08-19 16:45:12 -0700

    first commit

---
+++ trivia_frontend/src/index.js
@@ -0,0 +1,18 @@
+/* src/index.js */
+document.addEventListener('DOMContentLoaded', () => {
+    // const endPoint = 'http://localhost:3000/api/v1/questions';
+    // fetch(endPoint)
+    //   .then(res => res.json())
+    //   .then(json =>
+    //     json.forEach(question => {
+    //       const markup = `
+    //       <li>
+    //         <h3>${question.question}
+    //         </h3>
+    //       </li>`;
+  
+    //       document.querySelector('#questions-list').innerHTML += markup;
+    //     })
+    //   );
+  });
+
